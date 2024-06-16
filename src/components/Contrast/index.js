25a3f07813864fc6c5a7d8ff7681fbfbee731299
mazharzaka <mazharmohamed566@gmail.com>
2024-06-17 01:00:54 +0300

    convert color

--- src/components/Contrast/index.js
+++ src/components/Contrast/index.js
@@ -7,11 +7,13 @@ import {
   GrStatusGoodSmall,
   GrStatusWarningSmall,
 } from "react-icons/gr";
+import { HiOutlineCheckBadge } from "react-icons/hi2";
+import { MdOutlineErrorOutline } from "react-icons/md";
 
 function Contrast() {
   // const [picker, setPicker] = useState("#03a9f4");
-  const [color, setColor] = useColor("#00000");
-  const [color1, setColor1] = useColor("#03a9f4");
+  const [color, setColor] = useColor("#45D69B");
+  const [color1, setColor1] = useColor("#024D6B");
   const Fcolor = () => {
     const mcolor = document.querySelector(".con1");
     const mcolor1 = document.querySelector(".con2");
@@ -24,13 +26,21 @@ function Contrast() {
     const mcolor = document.querySelector(".con2");
     const mcolor1 = document.querySelector(".con1");
     mcolor.classList.toggle("active");
-    console.log(mcolor1.className === "w-52 mr-3 picker con1 active");
+    // console.log(mcolor1.className === "w-52 mr-3 picker con1 active");
     mcolor1.className !== "w-52 mr-3 picker con1 active"
       ? mcolor.parentElement.classList.add("justify-end")
       : mcolor.parentElement.classList.remove("justify-end");
 
     // &&
     //   mcolor.parentElement.classList.remove("justify-end");
+  };
+  const replace = () => {
+    const mcolor = document.querySelector(".con2");
+    const mcolor1 = document.querySelector(".con1");
+    mcolor.classList.remove("active");
+    mcolor1.classList.remove("active");
+    setColor(color1);
+    setColor1(color);
   };
   return (
     <div className="flex mt-5 flex-col min-w-96 mb-3">
@@ -45,7 +55,10 @@ function Contrast() {
               style={{ background: color.hex }}
             ></div>
           </div>
-          <div className="text-blue-300 text-2xl hover:text-blue-600 mt-5 ">
+          <div
+            className="text-blue-300 text-2xl hover:text-blue-600 mt-5 "
+            onClick={replace}
+          >
             <FaExchangeAlt />
           </div>
           <div className="flex w-32 items-center flex-col">
@@ -78,7 +91,10 @@ function Contrast() {
           />
         </div>
       </div>
-      <div className="w-full mt-5 bg-blue-100  rounded-sm ">
+      <div
+        className="w-full mt-5   rounded-sm "
+        style={{ color: color.hex, background: color1.hex }}
+      >
         <div className="text-3xl">Don't forget to donate to Palestine</div>
         <div className="text-lg text-center">
           free Palestine Until the end of life
@@ -98,6 +114,36 @@ function Contrast() {
             className="w-14 mr-2 ml-2"
           />
           <img src="./palestine (2).png" alt="palsetina" className="w-14" />
+        </div>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex  flex-col text-xl  text-[#31D190]">
+          <div className="flex items-center ">
+            <HiOutlineCheckBadge className="mr-1 text-2xl text-[#83E5BD]" />{" "}
+            Pass
+          </div>
+          <div className="text-black-600 text-[10px]">for 30px and above</div>
+        </div>
+        <div className="flex  flex-col text-xl  text-[#31D190]">
+          <div className="flex items-center">
+            <HiOutlineCheckBadge className="mr-1 text-2xl text-[#83E5BD]" />{" "}
+            Pass
+          </div>
+          <div className="text-black-600 text-[10px]">for 18px and less 30</div>
+        </div>
+        <div className="flex  flex-col text-xl  text-[#FA1631]">
+          <div className="flex items-center">
+            <MdOutlineErrorOutline className="mr-1 text-2xl text-[#FC475C]" />
+            failed
+          </div>
+          <div className="text-black-600 text-[10px]">for 8px and less 18</div>
+        </div>
+        <div className="flex  flex-col text-xl  text-[#31D190]">
+          <div className="flex items-center ">
+            <HiOutlineCheckBadge className="mr-1 text-2xl text-[#83E5BD]" />{" "}
+            Pass
+          </div>
+          <div className="text-black-600 text-[10px]">for icons</div>
         </div>
       </div>
     </div>
